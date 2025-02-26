72a0d9b27f2054713d006d493e7f6cc92b493cf9
Lucas Henrique de Figueiredo <111322948+imFigueiredo@users.noreply.github.com>
2025-02-26 01:45:13 -0300

    whatsapp_bot
    Bot para envio de mensagens automáticas para WhatsApp, desenvolvido em Python para fins de estudo.
    Para que funcione, é necessário alterar a planilha do Excel chamada "clientes" no formato .xlsx
    
    (Bot for sending automatic messages to WhatsApp, developed in Python for study purposes.
    For it to work, the Excel spreadsheet named "clientes" in .xlsx format must be modified.)
    
    Créditos ao canal: Dev Aprender | Jhonatan de Souza

---
+++ bot_whatsapp.py
@@ -0,0 +1,30 @@
+import openpyxl
+from urllib.parse import quote
+import webbrowser
+from time import sleep
+import pyautogui
+
+workbook = openpyxl.load_workbook('clientes.xlsx')
+
+pagina_clientes = workbook['Planilha1']
+
+for linha in pagina_clientes.iter_rows(min_row=2):
+    nome = linha[0].value
+    telefone = linha[1].value
+
+    mensagem = f'Olá {nome}, tudo bem? sou um bot teste desenvolvido por Lucas Henrique'
+
+    link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+    webbrowser.open(link_mensagem_whatsapp)
+    sleep(30) 
+
+    seta = pyautogui.locateCenterOnScreen('seta.png')
+    if seta is not None:
+        sleep(15) 
+        pyautogui.click(seta[0], seta[1])
+        sleep(15)
+    else:
+        print("Seta não encontrada na tela.")
+
+    pyautogui.hotkey('ctrl', 'w')
+    sleep(2) 
